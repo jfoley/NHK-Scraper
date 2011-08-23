190247206456502da56ea2ffb5443b57919ba01c
John Foley <jfoley@victorsandspoils.com>
2011-08-23 07:43:36 -0600

    Changed to point at latest episodes (not ones from 2010)
    Added thread pool for conversions.
    Changed how the recover() function works.

--- nhk_scraper.rb
+++ nhk_scraper.rb
@@ -49,11 +49,12 @@ end
 class NHKScraper
   DB_CONF = { :adapter => 'sqlite3', :database => 'nhk_shows.sqlite3' }
   LOG_FILE = "output.log"
-  MAX_THREADS = 5
+  MAX_DOWNLOAD_THREADS = 3
+  MAX_CONVERT_THREADS = 2
   ROOT_PATH = '高校講座'
 
   def initialize
-    @main_uri = URI.parse("http://www.nhk.or.jp/kokokoza/library/index.html")
+    @main_uri = URI.parse("http://www.nhk.or.jp/kokokoza/sitemap.html")
   end
 
   def load_database
@@ -68,8 +69,9 @@ class NHKScraper
 
         create_table :episodes do |t|
           t.string :name
-          t.string :url # this is only the URL of the page the episode is displayed on
+          t.string :url
           t.string :video_url
+          t.string :video_path
           t.integer :ep_number
           t.references :show
           t.boolean :downloaded, :default => false
@@ -86,13 +88,11 @@ class NHKScraper
 
     puts "opening main URL"
     doc = Nokogiri::HTML(open(@main_uri))
-    doc.css('a').each {|node|
-      if node['href'].match(/^2010\/tv.*$/)
-        url = node['href']
-        name = node.css('img').first['alt']
-        puts "url:#{url} name:#{name}"
-        Show.create(:name => name, :url => url)
-      end
+    doc.css('.site_left_02').first.css('a').each {|node|
+      url = node['href']
+      name = node.text
+      puts "url:#{url} name:#{name}"
+      Show.create(:name => name, :url => url)
     }
   end
 
@@ -115,7 +115,7 @@ class NHKScraper
             url = season_url.to_s.gsub(/index.*$/, '') + node['href'][2..-1]
             name = node.css('span').text
             puts "url:#{url} name:#{name}"
-            show.episodes.create(:url => url, :name => name, :ep_number => show.episodes.count + 1)
+            show.episodes.create(:url => url, :name => name, :ep_number => show.episodes.count + 1) if name != ''
           end
         }
       end
@@ -146,7 +146,7 @@ class NHKScraper
   end
 
   def download_shows
-    pool = Pool.new(MAX_THREADS)
+    pool = Pool.new(MAX_DOWNLOAD_THREADS)
 
     Episode.where(:downloaded => false).all.each do |episode|
       pool.schedule do
@@ -154,10 +154,10 @@ class NHKScraper
         puts "scheduling: #{video_file}\n"
         destination_path = File.join(ROOT_PATH, episode.show.name, video_file)
         File.delete(destination_path) if File.exists?(destination_path)
-        vlc_command =  "/Applications/VLC.app/Contents/MacOS/VLC -I dummy --sout='#transcode{vcodec=WMV2,vb=1024,acodec=a52,ab=192}:standard{mux=asf,dst=#{destination_path}},access=file}' #{episode.video_url} vlc://quit"
+        vlc_command =  "open -a VLC -I dummy --sout='#transcode{vcodec=WMV2,vb=1024,acodec=a52,ab=192}:standard{mux=asf,dst=#{destination_path}},access=file}' #{episode.video_url} vlc://quit"
         puts "executing: #{vlc_command}\n"
         `#{vlc_command} &>#{LOG_FILE}`
-        episode.update_attribute(:downloaded, true)
+        episode.update_attributes(:downloaded => true, :video_path => destination_path)
       end
     end
 
@@ -166,8 +166,32 @@ class NHKScraper
     end
 
     pool.shutdown
+  end
+    
+  def convert_shows
+    pool = Pool.new(MAX_CONVERT_THREADS)
 
-    recover
+    Episode.where(:converted => false).all.each do |episode|
+      pool.schedule do
+        match = episode.video_path.match(/\/(.*)\/(\d{2}).*\.asf$/)
+        next if match.nil?
+
+        show_name = match[1]
+        ep_number = match[2]
+
+        input_file = episode.video_path
+        basename = File.basename(episode.video_path, '.asf') + '.m4v' # remove the .asf and append .m4v
+        output_file = File.join(File.dirname(episode.video_path), basename)
+
+        handbrake_command = "HandbrakeCLI -i \"#{input_file}\" --preset=\"ipad\" -o \"#{output_file}\""
+        puts "running handbrake command: #{handbrake_command}"
+        `#{handbrake_command} &>#{LOG_FILE}`
+
+        episode.update_attribute(:converted, true)
+      end
+    end
+
+    pool.shutdown
   end
 
   def make_it_so
@@ -177,59 +201,45 @@ class NHKScraper
     get_video_urls
 
     create_directory_tree
-    # download_shows
-    convert_shows
-  end
 
-  def convert_shows
-    recover
-
-    Dir.glob( File.join(ROOT_PATH, '**', '*') ) do |file_path|
-      if File.file?(file_path) && File.size(file_path) > 78643200
-        match = file_path.match(/\/(.*)\/(\d{2})/)
-        next if match.nil?
-
-        show_name = match[1]
-        ep_number = match[2]
-
-        input_file = file_path
-        basename = File.basename(file_path, '.asf') + '.m4v' # remove the .asf and append .m4v
-        output_file = File.join(File.dirname(file_path), basename)
-
-        if File.exists?(output_file) && File.size(output_path) > 78643200
-          puts "skipping #{output_file}"
-          next
-        end
-        
-        handbrake_command = "HandbrakeCLI -i \"#{input_file}\" --preset=\"ipad\" -o \"#{output_file}\""
-        puts "running handbrake command: #{handbrake_command}"
-        `#{handbrake_command}`
-      end
+    while Episode.where("downloaded = 'f' OR converted = 'f'").exists?
+      recover
+      download_shows
+      convert_shows
     end
   end
 
   private
     def recover
     rerun = false
-    episode_ids = []
+    ids_to_download = []
+    ids_to_reconvert = []
 
     Dir.glob( File.join(ROOT_PATH, '**', '*') ) do |file_path|
       if File.file?(file_path) && File.size(file_path) < 78643200
-        match = file_path.match(/\/(.*)\/(\d{2})/)
+        match = file_path.match(/\/(.*)\/(\d{2}).*(asf|m4v)/)
         next if match.nil?
 
         show_name = match[1]
         ep_number = match[2]
 
-        episode_ids << Episode.where(:ep_number => ep_number).joins(:show).where(:shows => {:name => show_name}).collect(&:id)
+        episode_ids = Episode.where(:ep_number => ep_number).joins(:show).where(:shows => {:name => show_name}).collect(&:id)
         
+        if match[3] == 'asf'
+          ids_to_download << episode_ids
+        else
+          ids_to_reconvert << episode_ids
+        end
+
         File.delete(file_path)
       end
     end
 
-    puts "ids to redownload: #{episode_ids}"
-    episodes_updated = Episode.where(:id => episode_ids).update_all(:downloaded => false)
-    # download_shows if episodes_updated > 0
+    puts "ids to redownload: #{ids_to_download}"
+    Episode.where(:id => ids_to_download).update_all(:downloaded => false)
+
+    puts "ids to reconvert: #{ids_to_reconvert}"
+    Episode.where(:id => ids_to_reconvert).update_all(:converted => false)
   end
 end
 
